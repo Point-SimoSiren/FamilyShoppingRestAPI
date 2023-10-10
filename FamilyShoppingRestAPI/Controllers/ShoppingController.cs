4c7d6bb4a17ad3c2902bbdf45ca3f36023fef9ba
Simo Sirén <simo.siren1@careeria.fi>
2023-10-10 17:29:46 +0300

    Add project files.

---
+++ FamilyShoppingRestAPI/Controllers/ShoppingController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using FamilyShoppingRestAPI.Models;
+
+namespace FamilyShoppingRestAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ShoppingController : ControllerBase
+    {
+        private shoppingdbContext db = new();
+
+        [HttpGet]
+        public List<Item> GetItems()
+        {
+            List<Item> items = db.Items.ToList();
+            return items;
+        }
+
+        [HttpPost]
+        public ActionResult AddItem([FromBody] Item item)
+        {
+            db.Items.Add(item);
+            db.SaveChanges();
+            return Ok("Added: " + item.ItemName + " " + item.Pieces + "pieces");
+        }
+
+        [HttpDelete("{id}")]
+        public ActionResult removeItem(int id)
+        {
+            var item = db.Items.Find(id);
+            db.Items.Remove(item);
+            db.SaveChanges();
+            return Ok("Deleted item with id " + id + ". " + "Named " + item.ItemName);
+        }
+    }
+}
---
+++ FamilyShoppingRestAPI/Models/Item.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FamilyShoppingRestAPI.Models
+{
+    public partial class Item
+    {
+        public int Id { get; set; }
+        public string ItemName { get; set; } = null!;
+        public int Pieces { get; set; }
+        public DateTime TimeAdded { get; set; }
+    }
+}
